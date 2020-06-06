e392f71d729333610bb92f374d70eec0bc6ae70f
marcos170886 <65860883+marcos170886@users.noreply.github.com>
2020-06-05 22:22:24 -0300

    complemento API

---
+++ API/src/modules/users/infra/http/routes/users-routes.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+import UsersController from '../controllers/UsersController';
+import UserAvatarController from '../controllers/UserAvatarController';
+
+const usersRouter = Router();
+const upload = multer(uploadConfig);
+const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
+
+usersRouter.get('/', async (request, response) => {
+  return response.send();
+});
+
+usersRouter.post('/', usersController.create);
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  userAvatarController.update,
+);
+
+export default usersRouter;
