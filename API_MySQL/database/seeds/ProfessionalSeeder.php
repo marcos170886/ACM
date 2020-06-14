ce5208d6a6fb7be83515af1bd11c899db04828c8
marcos170886 <65860883+marcos170886@users.noreply.github.com>
2020-06-13 23:22:45 -0300

    Update ProfessionalSeeder.php

--- API_MySQL/database/seeds/ProfessionalSeeder.php
+++ API_MySQL/database/seeds/ProfessionalSeeder.php
@@ -13,86 +13,31 @@ class ProfessionalSeeder extends Seeder
     {
         DB::table('professionals')->insert([
             'id'=> 1,
-            'professionalName' => 'Joaquim',
-            'professionalPhone'=> '42999493934',
-            'professionalService' => 'Cabeleireiro',
+            'professionalName' => 'Dra. Joana D.',
+            'professionalPhone'=> '13988251183',
+            'professionalService' => 'Pediatra',
         ]);
 
         DB::table('professionals')->insert([
             'id'=> 2,
-            'professionalName' => 'Luisa',
-            'professionalPhone'=> '42999493934',
-            'professionalService' => 'Cabeleireiro',
+            'professionalName' => 'Dr. Jekyll',
+            'professionalPhone'=> '13988251183',
+            'professionalService' => 'Clinico',
         ]);
 
         DB::table('professionals')->insert([
             'id'=> 3,
-            'professionalName' => 'Jill',
-            'professionalPhone'=> '42999493934',
-            'professionalService' => 'Cabeleireiro',
+            'professionalName' => 'Mr. Hyde',
+            'professionalPhone'=> '13988251183',
+            'professionalService' => 'Ortopedista',
         ]);
 
         DB::table('professionals')->insert([
             'id'=> 4,
-            'professionalName' => 'Jeff',
-            'professionalPhone'=> '42999493934',
-            'professionalService' => 'Massoterapeuta',
+            'professionalName' => 'Dr. Love',
+            'professionalPhone'=> '13988251183',
+            'professionalService' => 'Cardiologista',
         ]);
 
-        DB::table('professionals')->insert([
-            'id'=> 5,
-            'professionalName' => 'Will',
-            'professionalPhone'=> '42999493934',
-            'professionalService' => 'Massoterapeuta',
-        ]);
-
-        DB::table('professionals')->insert([
-            'id'=> 6,
-            'professionalName' => 'Sonia',
-            'professionalPhone'=> '42999493934',
-            'professionalService' => 'Massoterapeuta',
-        ]);
-
-        DB::table('professionals')->insert([
-            'id'=> 7,
-            'professionalName' => 'Monicah',
-            'professionalPhone'=> '42999493934',
-            'professionalService' => 'Manicure',
-        ]);
-
-        DB::table('professionals')->insert([
-            'id'=> 8,
-            'professionalName' => 'Sander',
-            'professionalPhone'=> '42999493934',
-            'professionalService' => 'Manicure',
-        ]);
-
-        DB::table('professionals')->insert([
-            'id'=> 9,
-            'professionalName' => 'Bill',
-            'professionalPhone'=> '42999493934',
-            'professionalService' => 'Manicure',
-        ]);
-
-        DB::table('professionals')->insert([
-            'id'=> 10,
-            'professionalName' => 'Marcus',
-            'professionalPhone'=> '42999493934',
-            'professionalService' => 'Esteticista',
-        ]);
-
-        DB::table('professionals')->insert([
-            'id'=> 11,
-            'professionalName' => 'Ander',
-            'professionalPhone'=> '42999493934',
-            'professionalService' => 'Esteticista',
-        ]);
-
-        DB::table('professionals')->insert([
-            'id'=> 12,
-            'professionalName' => 'Manjerona',
-            'professionalPhone'=> '42999493934',
-            'professionalService' => 'Esteticista',
-        ]);
     }
 }
